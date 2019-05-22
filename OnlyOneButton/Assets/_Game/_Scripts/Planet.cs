d86518cdd3741edbbe9edc97fe83750857ff633f
tobias02121999 <hedgefoggames@outlook.com>
2019-05-22 23:02:12 +0200

    Update 0.1

---
+++ OnlyOneButton/Assets/_Game/_Scripts/Planet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    // Initialize the public variables
+    public GameObject playerObject;
+    public Transform rangeTransform;
+    public float gravity;
+
+    // Initialize the private variables
+    float range;
+    Transform playerTransform;
+    Player playerScript;
+
+    // Run this code once at the start
+    void Start()
+    {
+        // Get the range
+        range = rangeTransform.localScale.x / 2f;
+
+        // Get the player components
+        playerTransform = playerObject.transform;
+        playerScript = playerObject.GetComponent<Player>();
+    }
+
+    // Run this code every single frame
+    void FixedUpdate()
+    {
+        if (CheckInRange())
+            ApplyGravity();
+    }
+
+    // Check if the player is in range
+    bool CheckInRange()
+    {
+        float dist = Vector3.Distance(transform.position, playerTransform.position);
+        if (dist <= range)
+            return true;
+        else
+            return false;
+    }
+
+    // Pull the player in towards the planet
+    void ApplyGravity()
+    {
+        Vector3 relativePos = transform.position - playerTransform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+
+        Vector3 targetForward = rotation * Vector3.forward;
+
+        float dist = Vector3.Distance(transform.position, playerTransform.position);
+        float force = (range - dist) * gravity;
+
+        playerScript.rb.velocity += targetForward * gravity;
+    }
+}
---
+++ OnlyOneButton/Assets/_Game/_Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Initialize the public enums
+    public enum states { DEFAULT };
+
+    // Initialize the public variables
+    public states playerState;
+    public float thrustForce;
+    public ParticleSystem smokeParticleSystem;
+    public float rotationSpeed;
+    public Transform modelTransform;
+    public Transform cameraTransform;
+    public float lookRotationSpeed;
+
+    [HideInInspector]
+    public Rigidbody rb;
+
+    // Initialize the private variables
+    float rotationX;
+
+    // Run this code once at the start
+    void Start()
+    {
+        // Get the rigidbody component
+        rb = GetComponent<Rigidbody>();
+
+        // Set the cursor settings
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Run this code every single frame
+    void FixedUpdate()
+    {
+        RunState(playerState); // Run the current player state
+    }
+
+    // Run the current player state
+    void RunState(states state)
+    {
+        // Switch through the different player states
+        switch (state)
+        {
+            // The default player state
+            case states.DEFAULT:
+                Thrust(thrustForce); // Thrust the player forwards
+                Rotate(rotationSpeed); // Rotate the player around its own axis
+                break;
+        }
+    }
+
+    // Thrust the player forwards
+    void Thrust(float force)
+    {
+        // Add forward velocity to the player
+        if (Input.GetButton("Thrust"))
+        {
+            rb.velocity += (transform.forward * thrustForce);
+
+            if (!smokeParticleSystem.isEmitting)
+                smokeParticleSystem.Play();
+        }
+        else
+            smokeParticleSystem.Stop();
+    }
+
+    // Rotate the player around its own axis
+    void Rotate(float speed)
+    {
+        modelTransform.Rotate(0f, speed, 0f);
+
+        rotationX += Input.GetAxis("Mouse X") * lookRotationSpeed;
+        transform.rotation = Quaternion.Euler(rotationX, 90f, 90f);
+    }
+}
